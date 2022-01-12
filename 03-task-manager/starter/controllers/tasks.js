19cd72afb2cd9cc760cb76b47dd195f0acd0f6d1
montilloo <nikun2008@126.com>
2022-01-12 21:06:10 +0800

    获取所有任务;

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,6 +1,12 @@
 const Task = require('../models/Tasks');
 
-const getAllTasks = (req, res) => {
+const getAllTasks = async (req, res) => {
+	try{
+		const tasks = await Task.find({});
+		res.status(200).json({tasks});
+	} catch (error) {
+		res.status(500).json({mag: error});
+	}
 	res.send('all items from the file');
 }
 
