2c5f6023dc35562dd91749144242e9919caba567
montilloo <nikun2008@126.com>
2022-01-12 20:43:47 +0800

    添加基本的验证逻辑;

--- 03-task-manager/starter/models/Tasks.js
+++ 03-task-manager/starter/models/Tasks.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-	name: String, completed: Boolean
+	name: {
+		type: String,
+		required: [true, '必须提供姓名'],
+		trim: true,
+		maxlength: [20, '不能超过20个字符']
+	},
+	completed: {
+		type: Boolean,
+		default: false
+	}
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
